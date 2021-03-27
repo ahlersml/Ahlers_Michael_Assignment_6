6a77c283e10bd020deec02ffe22ec7fc728e2b33
ahlersml <70450921+ahlersml@users.noreply.github.com>
2021-03-27 11:09:54 -0400

    Add project files.

---
+++ Ahlers_Michael_Assignment_6/Controllers/NameController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Ahlers_Michael_Assignment_6.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class NameController : ControllerBase
+    {
+        private readonly ILogger<NameController> _logger;
+        public NameController(ILogger<NameController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public MyName Get()
+        {
+            return new MyName { Name = "Michael Ahlers" };
+        }
+    }
+}
---
+++ Ahlers_Michael_Assignment_6/Models/MyName.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace Ahlers_Michael_Assignment_6
+{
+    public class MyName
+    {
+        public string Name { get; set; }
+    }
+}
